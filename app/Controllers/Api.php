c2cb50005fbdf42567214ec7b3daa19f6ad0430a
James <totem.james@gmail.com>
2023-04-24 16:11:43 +0800

    api

---
+++ app/Controllers/Api.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Controllers;
+
+class Api extends BaseController
+{
+    // 新增會員
+    /**
+     * @api {put} /api/user/ 新增會員
+     * @apiName 新增會員
+     * @apiGroup User
+     * @apiVersion  1.0.0
+     * @apiSampleRequest /api/user/
+     * 
+     * 
+     * @apiBody {String} name 會員名稱
+     * @apiBody {String} email 會員信箱
+     * @apiBody {String} password 
+     * @apiBody {String} user_id 帳號
+     * @apiBody {String} birthday 生日
+     * @apiBody {String} phone_number 電話
+     * @apiBody {String} address 地址
+     * @apiBody {String} company_id 公司編號
+     * @apiBody {String} job_title 職稱
+     * @apiBody {String} department 
+     */
+
+    // 取得會員資料
+    // 編輯會員列表
+}
